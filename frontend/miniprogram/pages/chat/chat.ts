7390bb1fe55baa57eec6bf5d44d68a195a3e3611
nykla23 <2445895393@qq.com>
2026-03-22 21:58:43 +0800

    feat:添加聊天详情页

--- frontend/miniprogram/pages/chat/chat.ts
+++ frontend/miniprogram/pages/chat/chat.ts
@@ -5,7 +5,39 @@ Page({
    * 页面的初始数据
    */
   data: {
+    chatInfo: { nickname: "李四", taskTitle: "代买午餐" },
+    msgList: [
+      { id: 1, type: "receive", avatar: "/images/avatar-lisi.png", content: "你好，我可以帮你买午餐" },
+      { id: 2, type: "send", avatar: "/images/avatar-zhangsan.png", content: "太好了，麻烦你了，红烧肉在二楼" },
+      { id: 3, type: "receive", avatar: "/images/avatar-lisi.png", content: "好的，我马上去买" }
+    ],
+    inputMsg: "",
+    lastMsgId: "msg-3"
 
+  },
+
+  goBack() {
+    wx.navigateBack();
+  },
+
+  onInput(e: WechatMiniprogram.InputEvent) {
+    this.setData({ inputMsg: e.detail.value });
+  },
+
+  sendMsg() {
+    if (!this.data.inputMsg) return;
+    const newId = this.data.msgList.length + 1;
+    const newMsg = {
+      id: newId,
+      type: "send",
+      avatar: "/images/avatar-zhangsan.png",
+      content: this.data.inputMsg
+    };
+    this.setData({
+      msgList: [...this.data.msgList, newMsg],
+      inputMsg: "",
+      lastMsgId: `msg-${newId}`
+    });
   },
 
   /**
